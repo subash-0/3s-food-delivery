e3ef6e0ce96f8e098f2e5f92c4b1d0e786bd087f
subash-0 <subash67335@gmail.com>
2024-05-20 21:59:08 +0545

    headaer card for hero is added

--- src/components/HeaderCards.jsx
+++ src/components/HeaderCards.jsx
@@ -3,15 +3,74 @@ import { AiOutlineArrowRight } from 'react-icons/ai'
 
 const HeaderCards = () => {
   return (
-    <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
-      <div className='rounded-xl relative w-60 h-36'>
+    <div className='max-w-[1640px] mx-auto p-4 py-4 sm:py-12 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-3'>
+      <div className='rounded-xl relative sm:w-60 h-36'>
         <div  className='absolute w-full h-full bg-black/50 hover:bg-black/60 rounded-xl text-white flex flex-col'>
         <p  className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
         <p className='px-2 text-xl'>Through 8/26</p>
         <button  className='border-white bg-white mx-2 w-fit absolute bottom-2 px-2 text-black rounded-lg hover:bg-gray-200 duration-200 flex justify-center items-center gap-2'>Order Now <AiOutlineArrowRight /> </button>
         </div>
-        <img className='w-full h-full rounded-xl' src="https://images.pexels.com/photos/2347311/pexels-photo-2347311.jpeg?auto=compress&cs=tinysrgb&w=400" alt="/" />
+        <img className='w-full h-full rounded-xl object-cover' src="https://images.pexels.com/photos/2347311/pexels-photo-2347311.jpeg?auto=compress&cs=tinysrgb&w=400" alt="/" />
       </div>
+      
+        <div className='rounded-xl relative sm:w-60 h-36'>
+        <div  className='absolute w-full h-full bg-black/50 hover:bg-black/60 rounded-xl text-white flex flex-col'>
+        <p  className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+        <p className='px-2 text-xl'>Through 8/26</p>
+        <button  className='border-white bg-white mx-2 w-fit absolute bottom-2 px-2 text-black rounded-lg hover:bg-gray-200 duration-200 flex justify-center items-center gap-2'>Order Now <AiOutlineArrowRight /> </button>
+        </div>
+        <img className='w-full h-full rounded-xl object-cover' src="https://images.pexels.com/photos/106343/pexels-photo-106343.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+            <div className='rounded-xl relative sm:w-60 h-36'>
+        <div  className='absolute w-full h-full bg-black/50 hover:bg-black/60 rounded-xl text-white flex flex-col'>
+        <p  className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+        <p className='px-2 text-xl'>Through 8/26</p>
+        <button  className='border-white bg-white mx-2 w-fit absolute bottom-2 px-2 text-black rounded-lg hover:bg-gray-200 duration-200 flex justify-center items-center gap-2'>Order Now <AiOutlineArrowRight /> </button>
+        </div>
+        <img className='w-full h-full rounded-xl object-cover' src="https://images.pexels.com/photos/958546/pexels-photo-958546.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+            <div className='rounded-xl relative sm:w-60 h-36'>
+        <div  className='absolute w-full h-full bg-black/50 hover:bg-black/60 rounded-xl text-white flex flex-col'>
+        <p  className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+        <p className='px-2 text-xl'>Through 8/26</p>
+        <button  className='border-white bg-white mx-2 w-fit absolute bottom-2 px-2 text-black rounded-lg hover:bg-gray-200 duration-200 flex justify-center items-center gap-2'>Order Now <AiOutlineArrowRight /> </button>
+        </div>
+        <img className='w-full h-full rounded-xl object-cover' src="https://images.pexels.com/photos/674574/pexels-photo-674574.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+            <div className='rounded-xl relative sm:w-60 h-36'>
+        <div  className='absolute w-full h-full bg-black/50 hover:bg-black/60 rounded-xl text-white flex flex-col'>
+        <p  className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+        <p className='px-2 text-xl'>Through 8/26</p>
+        <button  className='border-white bg-white mx-2 w-fit absolute bottom-2 px-2 text-black rounded-lg hover:bg-gray-200 duration-200 flex justify-center items-center gap-2'>Order Now <AiOutlineArrowRight /> </button>
+        </div>
+        <img className='w-full h-full rounded-xl object-cover' src="https://images.pexels.com/photos/941869/pexels-photo-941869.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+            <div className='rounded-xl relative sm:w-60 h-36'>
+        <div  className='absolute w-full h-full bg-black/50 hover:bg-black/60 rounded-xl text-white flex flex-col'>
+        <p  className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+        <p className='px-2 text-xl'>Through 8/26</p>
+        <button  className='border-white bg-white mx-2 w-fit absolute bottom-2 px-2 text-black rounded-lg hover:bg-gray-200 duration-200 flex justify-center items-center gap-2'>Order Now <AiOutlineArrowRight /> </button>
+        </div>
+        <img className='w-full h-full rounded-xl object-cover' src="https://images.pexels.com/photos/1117862/pexels-photo-1117862.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+            <div className='rounded-xl relative sm:w-60 h-36'>
+        <div  className='absolute w-full h-full bg-black/50 hover:bg-black/60 rounded-xl text-white flex flex-col'>
+        <p  className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+        <p className='px-2 text-xl'>Through 8/26</p>
+        <button  className='border-white bg-white mx-2 w-fit absolute bottom-2 px-2 text-black rounded-lg hover:bg-gray-200 duration-200 flex justify-center items-center gap-2'>Order Now <AiOutlineArrowRight /> </button>
+        </div>
+        <img className='w-full h-full rounded-xl object-cover' src="https://images.pexels.com/photos/115740/pexels-photo-115740.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="/" />
+      </div>
+            <div className='rounded-xl relative sm:w-60 h-36'>
+        <div  className='absolute w-full h-full bg-black/50 hover:bg-black/60 rounded-xl text-white flex flex-col'>
+        <p  className='font-bold text-2xl px-2 pt-4'>Sun's Out, BOGO's Out</p>
+        <p className='px-2 text-xl'>Through 8/26</p>
+        <button  className='border-white bg-white mx-2 w-fit absolute bottom-2 px-2 text-black rounded-lg hover:bg-gray-200 duration-200 flex justify-center items-center gap-2'>Order Now <AiOutlineArrowRight /> </button>
+        </div>
+        <img className='w-full h-full rounded-xl object-cover' src="https://images.pexels.com/photos/2474661/pexels-photo-2474661.jpeg?auto=compress&cs=tinysrgb&w=600" alt="/" />
+      </div>
+            
+
     </div>
   )
 }
